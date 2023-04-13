3d5aa7df24ee5a3ce7fd2402ab2a43e43cf0402e
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-04-13 18:14:27 +0530

    program to print the pattern
    ****
    ***
    **
    *
    
    using the basic println() function

---
+++ Basic-java-programs/pattern.java
@@ -0,0 +1,8 @@
+class pattern{
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
