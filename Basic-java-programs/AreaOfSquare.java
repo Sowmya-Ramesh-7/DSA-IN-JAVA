7faed3b29f3fb061bbc7a6e64121edaf4014b252
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-04-15 14:55:34 +0530

    Create AreaOfSquare.java
    pow() function is used for s square
    
    java.lang package is a default package in Java therefore, there is no need to import it explicitly.
    i.e. without importing you can access the classes of this package.
    eg:Math, wrapper classes-byte, int ,....,void

---
+++ Basic-java-programs/AreaOfSquare.java
@@ -0,0 +1,7 @@
+public class AreaOfSquare {
+    public static void main(String args[]){
+        int side=20;
+        double area=Math.pow(side, 2); //math method is present in the java.lang package
+        System.out.println("The area of square of side "+side+" is "+area);
+    }
+}
