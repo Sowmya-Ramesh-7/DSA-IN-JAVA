45e1dc2a60fbe961d55477f657b18c4f4cb40d01
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-05-01 12:22:29 +0530

    Update TernaryOpEvenOdd.java

--- Conditional-Statements/TernaryOpEvenOdd.java
+++ Conditional-Statements/TernaryOpEvenOdd.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class TernaryOpEvenOdd{ 
     public static void main(String args[]) throws IOException{
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the number :")
+        System.out.print("Enter the number :");
         int num = Integer.parseInt(br.readLine());
         String type=(num%2==0)?"Number is even":"Number is odd";
         System.out.println(type);
