8d8c712100430ffaf1f40b1aa3804bf8bc7e9138
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-05-09 18:39:58 +0530

    Create LinearSearch.java
    >Tc=O(n)
      linear time complexity
    
    >output
    Enter the number of elements in array :
    4
    Enter the array elements :
    34
    08
    56
    79
    Enter the search element :
    8
    Array elements are :
    34 8 56 79
    8 is found at index :1 or position : 2

---
+++ Algorithms/Searching/LinearSearch.java
@@ -0,0 +1,36 @@
+import java.util.*;
+//using array
+public class LinearSearch {
+    //Linear Search
+    public static void linearSearch(int arr[],int key){
+        for(int i=0;i<arr.length;i++){
+            if(key==arr[i]){
+                System.out.println("\n"+key+" is found at index :"+i+" or position : "+(i+1));
+                return;
+            }
+        }
+        System.out.println("\nKey not found");
+        return;
+    }
+
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in array :");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the array elements :");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        System.out.println("Enter the search element :");
+        int key=sc.nextInt();
+        System.out.println("Array elements are :");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+            }
+        linearSearch(arr,key);
+    }
+    
+}
