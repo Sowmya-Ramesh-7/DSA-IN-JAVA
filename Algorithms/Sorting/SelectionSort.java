8ea282317ab2e3f51d90c092d64c5a342755d21d
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-06-01 23:03:49 +0530

    Create SelectionSort.java
    Selection sort : Pick the smallest element from unsorted part and put it in the begining
     TC=O(n2)
    
     Number of swaps is reduced
     Simple than Bubble Sort
    
    Output:
    Array {32,34,23,24,22} After sorting:
    22 23 24 32 34

---
+++ Algorithms/Sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+public class SelectionSort{
+
+/*
+ Selection sort : Pick the smallest element from unsorted part and put it in the begining 
+ TC=O(n2)
+
+ Number of swaps is reduced 
+ Simple than Bubble Sort
+ */
+    public static void Selection(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int small=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[small]){
+                    small=j;
+                }
+            }
+                
+            int temp=arr[small];
+            arr[small]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void main(String args[]){
+        int[] arr ={32,34,23,24,22};
+        System.out.println("Array {32,34,23,24,22} After sorting:");
+        Selection(arr);
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
