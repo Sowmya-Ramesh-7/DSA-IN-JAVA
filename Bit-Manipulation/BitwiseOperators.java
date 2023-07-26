eb3331fd525f5bd7ad59d2361b88f3513a4d213a
Sowmya-Ramesh-7 <Soumyaramesh2002@gmail.com>
2023-07-26 23:43:20 +0530

    Create BitwiseOperators.java
    &
    |
    ^
    ~
    <<
    >>

---
+++ Bit-Manipulation/BitwiseOperators.java
@@ -0,0 +1,14 @@
+
+
+public class BitwiseOperators {
+    public static void main(String args[]){
+        int a=1;
+        int b=0;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~b);
+        System.out.println(a<<2);
+        System.out.println(a>>2);
+    }
+}
