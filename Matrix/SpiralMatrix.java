28780f7f080b76706d8403854574af35d106a4a3
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-09-07 17:03:33 +0530

    Create SpiralMatrix.java
    Matrix :
    14 6 7
    2 4 5
    Spiral Matrix :14 6 7 5 4 2
    
    Corner Cases :
    1. while(startRow<endRow && startCol<endCol)
    works only for even number of rows or columns
    14 6 7
    2 4 5
    1 2 3
    Spiral Matrix :14 6 7 5 3 2 1 2
    that is, middle element will not be printed
    solution:  while(startRow<=endRow && startCol<=endCol)
    
    2. if(startRow==endRow) or if(startCol==endCol)  same row or column will be printed twice
    Matrix :
    14 6 7
    Spiral Matrix :14 6 7 6 14
    
    Hence
    if(startCol==endCol){
                        break;
                    }

---
+++ Matrix/SpiralMatrix.java
@@ -0,0 +1,54 @@
+
+public class SpiralMatrix {
+    public static void printSpiral(int matrix[][]){
+        int startRow=0,startCol=0;
+        int endRow=matrix.length-1;
+        int endCol=matrix[0].length-1;
+        System.out.print("Spiral Matrix :");
+        while(startRow<endRow && startCol<endCol){
+            //top
+            for(int j=startCol;j<=endCol;j++){
+
+                System.out.print(matrix[startRow][j]+" ");
+            }
+            //right
+            for(int i=startRow+1;i<=endRow;i++){
+                
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            //bottom
+            for(int j=endCol-1;j>=startCol;j--){
+                if(startRow==endRow){
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
+            //left
+            for(int i=endRow-1;i>=startCol+1;i--){
+                if(startCol==endCol){
+                    break;
+                }
+                System.out.print(matrix[i][startCol]+" ");
+            }
+
+            startRow++;
+            startCol++;
+            endCol--;
+            endRow--;
+        }
+        }
+        public static void printMatrix(int matrix[][]){
+            System.out.println("Matrix :");
+            for(int i=0;i<matrix.length;i++){
+                for(int j=0;j<matrix[0].length;j++){
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+        public static void main(String args[]){
+            int matrix[][]={{14,6,7},{2, 4,5},{1,2,3}};
+            printMatrix(matrix);
+            printSpiral(matrix);
+    }
+}
