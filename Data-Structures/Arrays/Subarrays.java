3277b0f285c07eadd44edefd10037d713d6ea06a
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-12-06 15:22:10 +0530

    Print Subarrays of an Array
    Subarrays is continous part of array
    Total number of Subarrays of an array =n*(n+1)/2
    Time complexity=O(n3)
    Input:  arr={2,4,6,8,10}
    
    Output:
    
    2
    2 4
    2 4 6
    2 4 6 8
    2 4 6 8 10
    
    4
    4 6
    4 6 8
    4 6 8 10
    
    6
    6 8
    6 8 10
    
    8
    8 10
    
    10

---
+++ Data-Structures/Arrays/Subarrays.java
@@ -0,0 +1,17 @@
+public class Subarrays {
+    public static void printSubarrays(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n;i++){       //start
+            for(int j=i;j<=n;j++){      //end
+                for(int k=i;k<j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={2,4,6,8,10};
+        printSubarrays(arr);
+    }
+}
